437ffc965214a0c7f3819ef32cff230f69788f94
xxx633 <vittorioxiong@gmail.com>
2025-03-17 14:05:02 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 import threading
 import time
 import requests
+import asyncio
+import aiohttp
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,19 +14,20 @@ def health_check():
 
 def keep_awake():
     url="http://small-viola-vanny-f71cc402.koyeb.app/health"
-    while True:
-        try:
-            response=requests.get(url)
-            print(f"Sent keep-alive request,status code:{response.status_code}")
-        except Exception as e:
-            print(f"Request failed:{e}")
-
-        time.sleep(1800)
+    async with aiohttp.ClientSession() as session:
+        while True:
+            try:
+                async with session.get(url) as response:
+                    print(f"Sent keep-alive request,status code:{response.status_code}")
+            except Exception as e:
+                print(f"Request failed:{e}")
+    
+            await asyncio.sleep(900)
 
 def run_server():
+    loop=asyncio.get_event_loop()
     try:
-        keep_awake_thread=threading.Thread(target=keep_awake,daemon=True)
-        keep_awake_thread.start()
+        loop.create_task(keep_awake())
         
         app.run(host="0.0.0.0", port=8000)
     except KeyboardInterrupt:
