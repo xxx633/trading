785612944cb9aeaafd88db7fd12bc2c0448284c9
xxx633 <vittorioxiong@gmail.com>
2025-12-29 16:59:25 +0100

    Update kriora.py

--- kriora.py
+++ kriora.py
@@ -117,7 +117,7 @@ Decide BUY, SELL, or NO_TRADE based on this data."""
     
     try:
         completion = client.chat.completions.create(
-            model="deepseek/deepseek-v3.2-think",
+            model="deepseek/deepseek-r1-0528",
             messages = [{"role": "system","content":SYSTEM_PROMT},
                         {"role": "user","content":USER_PROMT}],
             temperature=0
