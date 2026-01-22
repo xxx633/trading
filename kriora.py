61b8b8d10d78305ae7317de11dce90238f27c0a6
xxx633 <vittorioxiong@gmail.com>
2026-01-22 08:37:56 +0100

    Update kriora.py

--- kriora.py
+++ kriora.py
@@ -68,11 +68,11 @@ def place_order(cst,token,sig,df):
     size=1
 
     if sig == "BUY":
-        tp = current_price + 3
-        sl=current_price - 9
+        tp = current_price + 2
+        sl=current_price - 5
     else:
-        tp = current_price - 3
-        sl=current_price + 9
+        tp = current_price - 2
+        sl=current_price + 5
 
     order = {
         "epic": "GOLD",
